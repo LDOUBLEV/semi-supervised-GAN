e8bdfe5e64daf2bed0c745053198342f14277ce0
Double_V <liuvv0203@163.com>
2017-11-13 22:23:31 +0800

    Update main.py

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--model', type=str, default='DCGAN', help='DCGAN or WGAN-GP')
 parser.add_argument('--trainable', type=bool, default=False,help='True for train and False for test')
 parser.add_argument('--load_model', type=bool, default=True, help='True for load ckpt model and False for otherwise')
+parser.add_argument('--label_num', type=int, default=200, help='the num of labled images we use')
 args = parser.parse_args()
 
 def main():
